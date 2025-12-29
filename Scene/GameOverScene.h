2d4177df7609753e332466c2436712f90a3a2ffe
Yihao Fu <1546697366@qq.com>
2025-12-30 02:00:17 +0800

    Refactor GameOverScene header comments and formatting

--- Scene/GameOverScene.h
+++ Scene/GameOverScene.h
@@ -8,39 +8,39 @@
 class GameOverScene : public cocos2d::Layer
 {
 public:
-    // 锟斤拷锟斤拷锟斤拷锟斤拷
+    // 创建场景
     static cocos2d::Scene* createScene();
 
-    // 锟斤拷始锟斤拷
+    // 初始化
     virtual bool init() override;
 
     CREATE_FUNC(GameOverScene);
 
 private:
-    static int s_currentSessionDeathCount;  // 锟斤拷锟轿会话锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
-    // UI元锟斤拷
+    static int s_currentSessionDeathCount;  // 当前会话的死亡次数
+    // UI 元素
     cocos2d::Label* m_gameOverLabel;
     cocos2d::ui::Button* m_restartButton;
     cocos2d::ui::Button* m_menuButton;
     cocos2d::LayerColor* m_background;
 
-    // 锟斤拷锟教硷拷锟斤拷锟斤拷
+    // 键盘事件监听
     cocos2d::EventListenerKeyboard* m_keyboardListener;
 
-    // 锟斤拷锟斤拷UI
+    // 构建 UI
     void createUI();
 
-    std::string GameOverScene::getDeathDescription(int count);
+    std::string getDeathDescription(int count);
 
-    // 锟斤拷钮锟截碉拷
+    // 按钮回调
     void onRestartClicked(cocos2d::Ref* sender);
     void onMenuClicked(cocos2d::Ref* sender);
 
-    // 锟斤拷锟斤拷锟铰硷拷
+    // 键盘处理
     void initKeyboardListener();
     void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
 
-    // 锟斤拷锟斤拷锟斤拷锟斤拷
+    // 析构
     virtual ~GameOverScene();
 };
 
